98030927159883a068a7194313470f6ee3c7f7c6
TichaTin <meiliizzsuju@gmail.com>
2023-02-16 16:32:57 +1100

    GET request: Filter By email ID

--- routes/users.js
+++ routes/users.js
@@ -28,17 +28,12 @@ router.get("/",(req,res)=>{
   res.send(users);
 });
 
+
 // GET request: Filter By email ID
 router.get("/:email", (req, res) => {
   const email = req.params.email;
   let filtered_users = users.filter((user) => user.email === email);
   res.send(filtered_users);
-});
-
-// GET by specific ID request: Retrieve a single user with email ID
-router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
 
